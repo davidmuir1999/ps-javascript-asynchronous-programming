19511f11eec79e6595b18048b5659b4ced0f62fe
DMuir <david.muir@hymans.co.uk>
2024-04-17 12:33:25 +0100

    Understanding and Consuming Promises

--- public/orders.js
+++ public/orders.js
@@ -1,23 +1,15 @@
 let statuses = [];
-let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://localhost:3000/api/orderStatuses");
-xhr.onload = () => {
-  statuses = JSON.parse(xhr.responseText);
+axios.get("http://localhost:3000/api/orderStatuses").then(({data}) => {
+  statuses = data; //gets order status
 
-  let xhr2 = new XMLHttpRequest();
-  xhr2.open("GET", "http://localhost:3000/api/orders");
-  xhr2.onload = () => {
-    const orders = JSON.parse(xhr2.responseText);
-    const fullOrders = orders.map((o) => {
-      o.orderStatus = statuses.find(
-        (s) => s.id === o.orderStatusId
-      ).description;
-      return o;
-    });
-
-    showOrderList("#order-list", fullOrders);
-  };
-  xhr2.send();
-};
-xhr.send();
+  return axios.get("http://localhost:3000/api/orders"); //then gets all orders
+}).then(({data}) => {
+  let orders = data.map((o) => { //loops over those orders, as assigns order status description
+    return {
+      ...o,
+      orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
+    };
+  })
+  showOrderList('#order-list', orders);
+}).catch((err) => showError('#order-list', err));//catching an error
