228f91b89a58079367af15cdb1130a442193493a
davidmuir1999 <david.bme.muir2017@gmail.com>
2024-04-18 13:11:31 +0100

    Async Await

--- public/orders.js
+++ public/orders.js
@@ -1,33 +1,23 @@
-let statusReq = axios.get("http://localhost:3000/api/orderStatuses");
-let addressReq = axios.get("http://localhost:3000/api/addresses");
-
-let statuses = [];
-let addresses = [];
-showWaiting();
-
-Promise.allSettled([statusReq, addressReq]).then(([statusRes, addressRes]) => {
-  if(statusRes.status === 'fulfilled'){
-    statuses = statusRes.value.data;
-  } else {
-    window.alert('Order status error: '+ statusRes.reason.message);
+//this change will highlight the difference when using async await
+async function get(){
+  try{
+    const statusReq = axios.get('http://localhost:3000/api/orderStatuses');
+    const orderReq = axios.get('http://localhost:3000/api/orders');
+    
+    const {data: statuses} = await statusReq;
+    const {data: order} = await orderReq;
+    
+    const orders = order.map((o) => {
+      return {
+        ...o,
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
+      }
+    });
+    showOrderList('#order-list', orders);
+  } catch {
+    showError('#order-list', error);
   }
-  if(addressRes.status === 'fulfilled'){
-    addresses = addressRes.value.data;
-  } else {
-    window.alert('Order status error: '+ addressRes.reason.message);
-  }
-  return axios.get("http://localhost:3000/api/orders");
-})
-.then(({data}) => {
-  let orders = data.map((d) => {
-    const addr = addresses.find((a) => a.id === d.shippingAddress);
-    return {
-      ...d,
-      orderStatus: statuses.find((s) => s.id === d.orderStatusId).description,
-      shippingAddressText: `${addr.street} ${addr.city}, ${addr.state} ${addr.zipcode}`
-    }
-  })
-  showOrderList('#order-list', orders);
-}).catch((err) => showError('#order-list', err)).finally(() => {
-  setTimeout(hideWaiting, 1500)
-});//catching an error
+  
+}
+
+get();
